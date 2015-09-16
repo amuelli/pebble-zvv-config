025cddd2865520e27c8830ff392cb71277a31a65
Alex Mülli <amuelli@gmail.com>
2015-09-16 15:45:54 +0200

    fix encoding problem

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/stations', function(req,res) {
-  var keyword = req.query.q;
+  var keyword = encodeURI(req.query.q);
   request('http://www.timeforcoffee.ch/api/zvv/stations/' + keyword, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(JSON.parse(body).stations);
